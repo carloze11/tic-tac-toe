08c44c6c1fbf54b597a9099f23f19a03cb8315b0
Carlos Estrada <cestrada1109@gmail.com>
2022-08-26 19:45:56 -0500

    Fix bugs and add winner announcement

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const gameBoard = (() => {
   //*****************************************************************************
   // Creating variables for each div to check for winner later
   // There is most likely a better way to do this but can't think of anything atm
+  const winner = document.querySelector('#winner');
   const block1 = document.querySelector('#block1');
   const block2 = document.querySelector('#block2');
   const block3 = document.querySelector('#block3');
@@ -14,6 +15,27 @@ const gameBoard = (() => {
   //****************************************************************************
   
   const markersArray = [{}];
+  
+  //Start and reset the game 
+  const startBtn = () => {
+  let btn = document.querySelector('.play');
+  let resetBtn = document.querySelector('.reset')
+  btn.addEventListener('click', () => {
+    gameBoard.playGame();
+    btn.setAttribute('hidden', 'true')
+    resetBtn.removeAttribute('hidden')
+  })
+}
+
+const resetBtn = () => {
+  let btn = document.querySelector('.reset')
+  btn.addEventListener('click', () => {
+    location.reload(false);
+  })
+}
+  
+  
+  
   
   //when clicked, grab the parent div (done)
   //check if it's been used
@@ -29,21 +51,21 @@ const gameBoard = (() => {
         if (chosenBlock.textContent !== ""){
           return alert('Choose an empty block, ya filthy animal!');
         };        
-        if (lastPlayer === undefined || lastPlayer === 'player2') {
+        if (lastPlayer === undefined || lastPlayer === player2.name) {
           gameBoard.addMarker(player1);
-        } else if (lastPlayer === 'player1'){
+        } else if (lastPlayer === player1.name){
           gameBoard.addMarker(player2);
         };
         // Update last players' reference
         lastPlayer = markersArray[markersArray.length-1].name;
         
         //Mark up the board
-        if ((lastPlayer === 'player1' || lastPlayer === undefined) && chosenBlock.textContent === ""){
+        if ((lastPlayer === player1.name || lastPlayer === undefined) && chosenBlock.textContent === ""){
           chosenBlock.textContent = "X";
-          chosenBlock.style.color = "darkred";
-        } else if (lastPlayer === 'player2' && chosenBlock.textContent === "") {
+          chosenBlock.style.color = "blueviolet";
+        } else if (lastPlayer === player2.name && chosenBlock.textContent === "") {
           chosenBlock.textContent = "O";
-          chosenBlock.style.color = "darkblue";
+          chosenBlock.style.color = "darkturquoise";
         };   
         
         //Check for winner
@@ -52,19 +74,89 @@ const gameBoard = (() => {
         //I am going to check for winner using if statements
         //I understand this would've been easier had i used a table system...
         if (block1.textContent !== "" && block1.textContent === block2.textContent && block1.textContent === block3.textContent){
-          console.log(`${block1.textContent} Wins!`);
+          if (block1.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (block1.textContent !== "" && block1.textContent === block4.textContent && block1.textContent === block7.textContent){
-          console.log(`${block1.textContent} Wins!`);
+          if (block1.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (block1.textContent !== "" && block1.textContent === block5.textContent && block1.textContent === block9.textContent){
-          console.log(`${block1.textContent} Wins!`);
+          if (block1.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (block2.textContent !== "" && block2.textContent === block5.textContent && block2.textContent === block8.textContent){
-          console.log(`${block2.textContent} Wins!`);
+          if (block2.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (block3.textContent !== "" && block3.textContent === block6.textContent && block3.textContent === block9.textContent){
-          console.log(`${block3.textContent} Wins!`);
+          if (block3.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (block3.textContent !== "" && block3.textContent === block5.textContent && block3.textContent === block7.textContent){
-          console.log(`${block3.textContent} Wins!`);
+          if (block3.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
+        } else if (block4.textContent !== "" && block4.textContent === block5.textContent && block4.textContent === block6.textContent){
+          if (block4.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
+        } else if (block7.textContent !== "" && block7.textContent === block8.textContent && block7.textContent === block9.textContent){
+          if (block7.textContent === 'X'){
+            winner.textContent = `${player1.name} WINS!!!`;
+            winner.style.color = 'blueviolet';
+          } else {
+            winner.textContent = `${player2.name} WINS!!!`;
+            winner.style.color = 'darkturquoise';
+          }
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         } else if (markersArray.length >= 10) {
-          console.log('Tie!')
+          winner.textContent = 'Tie!';
+          winner.removeAttribute('hidden');
+          winner.style.display = 'flex';
         }
         //*****************************************************************************
       })
@@ -77,7 +169,10 @@ const gameBoard = (() => {
   // Public
   const addMarker = (mark) => markersArray.push(mark);
   const playGame = () => chooseSquare();
-  return {markersArray, addMarker, playGame};
+  const startGame = () => startBtn();
+  const resetGame = () => resetBtn();
+  
+  return {markersArray, addMarker, playGame, startGame, resetGame};
 })();
 
 
@@ -88,7 +183,12 @@ const Player = (name, mark) => {
   return {name, mark}
 };
 
-const player1 = Player('player1', 'X');
-const player2 = Player('player2', 'O');
+const player1 = Player('cindy', 'X');
+const player2 = Player('eevee', 'O');
 
-gameBoard.playGame();
+
+
+
+gameBoard.startGame();
+gameBoard.resetGame();
+
